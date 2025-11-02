609553598f4a525632d9cb4b11ae8787283cfe7f
Trung Vu <trung.vd225161@sis.hust.edu.vn>
2025-11-02 15:10:04 +0700

    feat: enhance user model and signup/login to include fullName validation and response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,9 +7,9 @@ import BrevoProvider from "../config/brevo.js";
 
 //Sign up
 export const signUp = async (req, res) => {
-  const { userName, email, password } = req.body;
+  const { userName, fullName, email, password } = req.body;
   try {
-    if (!userName || !email || !password) {
+    if (!userName || !fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
     // Kiểm tra trùng email
@@ -25,6 +25,7 @@ export const signUp = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       userName,
+      fullName,
       email,
       password: hashedPassword,
     });
@@ -33,7 +34,12 @@ export const signUp = async (req, res) => {
       await newUser.save();
 
       res.status(201).json({
-        user: { id: newUser.id, name: newUser.name, email: newUser.email },
+        user: {
+          id: newUser.id,
+          userName: newUser.userName,
+          fullName: newUser.fullName,
+          email: newUser.email,
+        },
       });
     } else {
       res.status(400).json({ message: "Lỗi", error: "Invalid user data!" });
@@ -92,7 +98,12 @@ export const login = async (req, res) => {
 
     res.status(200).json({
       message: "Login successful",
-      user: { id: user.id, name: user.name, email: user.email },
+      user: {
+        id: user.id,
+        userName: user.userName,
+        fullName: user.fullName,
+        email: user.email,
+      },
       accessToken,
     });
   } catch (e) {
